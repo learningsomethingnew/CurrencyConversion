45aaf6f0336c05cb3ea3da19e590431636b98f6c
Nic Colley <nic.colley@live.com>
2016-04-04 14:16:43 -0400

    Initial project setup with classes of currency, currency converter, and currency trader

---
+++ CurrencyTrader.py
@@ -0,0 +1,11 @@
+# CurrencyTrader must be initialized with two CurrencyConverter
+# objects from two different points in time, plus a starting
+# Currency. CurrencyTrader must have a method which returns the
+# best currency investment over that span of time.
+
+
+class CurrencyTrader():
+
+    def __init__(self):
+        pass
+

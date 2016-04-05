b7b1856eb9bfc36d50f137a8f1d4690c697cd52a
Nic Colley <nic.colley@live.com>
2016-04-04 21:53:59 -0400

    started to work on the symbols. Got a few converted over

--- CurrencyClasses/Currency.py
+++ CurrencyClasses/Currency.py
@@ -9,7 +9,7 @@
 # Must be able to be subtracted by another Currency object with the same currency code.✓
 # Must raise a DifferentCurrencyCodeError when you try to add or subtract two Currency
 #      objects with different currency codes. ✓
-# Must be able to be multiplied by an int or float and return a Currency object.
+# Must be able to be multiplied by an int or float and return a Currency object. ✓
 # Currency() must be able to take one argument with a currency symbol embedded in it, like
 #   "$1.20" or "€ 7.00", and figure out the correct currency code. It can also take two
 #   arguments, one being the amount and the other being the currency code.
